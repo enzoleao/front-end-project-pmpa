bf1fcbf81a8d15106ce3beba8da4010d63bcfa1e
enzoleao <enzopinheiro6@gmail.com>
2023-06-23 16:23:05 -0300

    adicionando novas estilizacoes

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -30,7 +30,7 @@ export function CointsCard (props: any) {
   }
     return( 
         <>
-            <Card style={{minWidth: 150, minHeight: 200, display:'flex', flexDirection:'column', alignItems: 'center', justifyContent:'center', boxShadow:'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px' }}>
+            <Card style={{minWidth: 190, minHeight: 200, display:'flex', flexDirection:'column', alignItems: 'center', justifyContent:'center', boxShadow:'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px', backgroundColor: "#003366", color:"#ffffff", fontSize: "18px",fontFamily: "Georgia" }}>
                 <div className='flex flex-col gap-4 items-center'>
                     <p>{props.name}</p>
                     <p>{props.vagas} VAGAS</p>
@@ -39,19 +39,19 @@ export function CointsCard (props: any) {
                     </button>
                 </div>
             </Card>
-            <Modal width={1000} style={{maxWidth: 'none'}} footer={false} title={props.name} open={isModalOpen} onCancel={handleCancel}>
+            <Modal  width={1000} style={{maxWidth: 'none'}} footer={false} title={props.name} open={isModalOpen} onCancel={handleCancel}>
                 <Form
-                    form={form}
+                    form={form} 
                     onFinish={handleSumbit}
                     layout='vertical'
                     name="basic"
                     autoComplete="off"
-                    style={{width: '100%', alignItems: 'flex-start', justifyContent:'flex-start', display: 'flex', flexDirection: 'column', gap:'10px'}}
+                    style={{width: '100%', backgroundColor:"#333c6b", alignItems: 'flex-start', justifyContent:'flex-start', display: 'flex', flexDirection: 'column', gap:'10px', color:"red"}}
                 >
                     <Form.Item
                         label="RG"
                         name="rg"
-                        style={{width:'100%'}}
+                        style={{width:'100%', backgroundColor:"#333c6b", borderTop:"1px solid"}}
                         rules={[{ required: true, message: 'Por favor, insira seu RG!' }]}
                     >
                         <Input />
@@ -59,15 +59,15 @@ export function CointsCard (props: any) {
                     <Form.Item
                         label="OPM"
                         name="opm"
-                        style={{width:'100%'}}
+                        style={{width:'100%', borderTop:"1px solid #fff", backgroundColor:"#333c6b"}}
                         rules={[{ required: true, message: 'Por favor, insira sua OPM!'}]}
                     >
-                        <Card style={{width:'100%'}}>
-                            <Radio.Group style={{width:'100%', display:'flex', flexWrap:'wrap', justifyContent:'center'}} value={value} onChange={(e: any)=>setValue(e.target.value)}>
+                        <Card style={{width:'100%', backgroundColor:"#2b5b93", border:"1px solid #fff" }}>
+                            <Radio.Group style={{width:'100%', backgroundColor: "#333c6b", display:'flex', flexWrap:'wrap', justifyContent:'center', border:"1px solid #fff"}} value={value} onChange={(e: any)=>setValue(e.target.value)}>
                                 {props.opms.map((i: any)=>{
                                     return (
-                                    <Radio style={{display:'flex', flexDirection:'column-reverse', maxWidth: 250, gap: 5}} key={i.id} disabled={i.vacancies ===0 ? true: false} value={i.id}>
-                                        <Card style={{minWidth:250}}>
+                                    <Radio style={{display:'flex', flexDirection:'column-reverse', maxWidth: 250, gap: 5, margin:"5px"}} key={i.id} disabled={i.vacancies ===0 ? true: false} value={i.id}>
+                                        <Card style={{minWidth:250,fontSize:'38px', backgroundColor:"#2b5b93", color: "#fff", fontFamily:"Georgia"}}>
                                             <p>{i.name}</p>
                                             <p>{i.vacancies} VAGAS</p>
                                         </Card> 
@@ -77,8 +77,8 @@ export function CointsCard (props: any) {
                             </Radio.Group>
                         </Card>
                     </Form.Item>
-                    <Form.Item style={{display:"flex", width:'100%', justifyContent:'flex-end'}} >
-                        <Button  htmlType="submit">
+                    <Form.Item style={{display:"flex", width:'100%', justifyContent:'center'}} >
+                        <Button  htmlType="submit" style={{backgroundColor:"#fff", border:"1px solid #fff"}}>
                             CONFIRMAR
                         </Button>
                     </Form.Item>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,25 @@
 import styles from './styles.module.scss'
+import Image from 'next/image'
+import logoPm from "/public/Brasão\ PMPA\ 2.png"
+import logodgp from "/public/DGP.png"
 export function Header () {
     return (
     <div className={styles.headerWrapper}>
-        <div className={styles.headerContainer}></div>
+        
+
+        <div className={styles.headerContainer}>
+        <Image
+         src={logoPm}
+         className={styles.imgHeader}
+         alt="DGP" 
+         />
+         <h1 className={styles.nomeHeader}>Polícia Militar do Pará</h1>
+         <Image
+         src={logodgp}
+         className={styles.imgHeader}
+         alt="PM" 
+         />
+        </div>
     </div>
     )
 }
